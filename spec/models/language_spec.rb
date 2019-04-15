836d6b2aea9dd9ccb70d4ee699b5e81bfe6f97d9
hai phan <hai.phan@tech.reapra.sg>
2019-04-15 16:03:53 +0700

    Getting language that no one speaks

--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -18,4 +18,19 @@ RSpec.describe Language, type: :model do
     language = Language.new(code: subject.code)
     expect(language).to_not be_valid
   end
+
+  describe 'language_no_speaking' do
+    before do
+      create(:guide_language)
+    end
+
+    it 'returns empty array when all language are used' do
+      expect(Language.language_no_speaking).to match_array([])
+    end
+
+    it 'returns array code when language that no one speaks.' do
+      language_no_speaking = create(:language)
+      expect(Language.language_no_speaking).to match_array([language_no_speaking.code])
+    end
+  end
 end
