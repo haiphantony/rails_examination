f09c6e57b88bee2da3b9df8dacc2d1052a28a00a
haiphantony <hai.phan@tech.reapra.sg>
2019-04-16 07:28:53 +0700

    Revert "(C) Frontend"

--- app/serializers/review/detail_serializer.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Review
-  class DetailSerializer < ActiveModel::Serializer
-    attributes :id, :score, :comment
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'guides#index'
-  resources :guides, only: [:index, :show]
-
-
-  # api
-  namespace :api do
-    namespace :v1 do
-      resources :reviews, only:[:create, :destroy]
-    end
-  end
+  resources :guides, only: [:index]
 end
